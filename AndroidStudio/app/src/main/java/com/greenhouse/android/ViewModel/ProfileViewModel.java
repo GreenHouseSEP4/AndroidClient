a8acd8de7a116250abbbe6f03c27f8597b2d44fa
Tamassp <petertamas05@gmail.com>
2021-12-16 09:38:11 +0100

    profile view - names changed, we still need to get the current user

---
+++ AndroidStudio/app/src/main/java/com/greenhouse/android/ViewModel/ProfileViewModel.java
@@ -0,0 +1,27 @@
+package com.greenhouse.android.ViewModel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.greenhouse.android.Model.DeviceRepository;
+import com.greenhouse.android.Model.UserRepository;
+import com.greenhouse.android.Wrappers.Device;
+import com.greenhouse.android.Wrappers.User;
+
+import java.util.List;
+
+public class ProfileViewModel extends ViewModel {
+    UserRepository repository;
+    public ProfileViewModel() {
+        repository = UserRepository.getInstance();
+    }
+
+    public void updateUser(User user){repository.login(user);}
+
+
+
+}
